a32a717f572487fa7f49d6e0d1af53a0912488ef
joergent <joergent@users.noreply.github.com>
2023-01-30 17:07:04 +0100

    Fetch single values and missing fields added
    Now a single or a few variables can be fetched, not only a dump of all of the fields
    Fields of Smart pric/energy added as well as other missing fields added

--- get_metrics.py
+++ get_metrics.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
 import json
 import os
 import re
@@ -18,6 +20,9 @@ TYPE_FLOW = "flow"
 TYPE_PERCENTAGE = "percentage"
 TYPE_PRESSURE = "pressure"
 TYPE_FREQUENCY = "frequency"
+TYPE_CURRENCY = "currency";
+UNIT_DKCURRENCY = "\u00f8re"; # DKK 0.01 unit
+UNIT_CURRENCY = UNIT_DKCURRENCY;
 UNIT_CELSIUS = "C"
 UNIT_AMPERES = "A"
 UNIT_DEGREE_MINUTES = "DM"
@@ -71,7 +76,25 @@ definition_groups = {
         44304: {"name": "pool, compr. only.", "type": TYPE_ENERGY, "unit": UNIT_KILOWATT_HOURS},
         40072: {"name": "flow BF1", "type": TYPE_FLOW, "unit": UNIT_LITERS_PER_MINUTE},
     },
-    # Soft inputs (AUX1-AUX5) not implemented
+    "smart_energy_source": {
+        41366: {"name": "compressor", "type": TYPE_DIMENSIONLESS, "unit": UNIT_DEGREE_MINUTES},
+        41427: {"name": "price, electricity", "type": TYPE_CURRENCY, "unit": UNIT_CURRENCY},
+    },
+    "smart_price_adaptation": {
+        44896: {"name": "heating", "type": TYPE_DIMENSIONLESS, "unit": UNIT_NONE},
+        44897: {"name": "hotwater", "type": TYPE_DIMENSIONLESS, "unit": UNIT_NONE},
+        44908: {"name": "hotwater", "type": TYPE_DIMENSIONLESS, "unit": UNIT_NONE},
+        10069: {"name": "price of electricity", "type": TYPE_CURRENCY, "unit": UNIT_CURRENCY},
+
+    },
+    "soft in/outputs": {
+        47411: {"name": "AUX1", "type": TYPE_DIMENSIONLESS, "unit": UNIT_NONE},
+        47410: {"name": "AUX2", "type": TYPE_DIMENSIONLESS, "unit": UNIT_NONE},
+        47409: {"name": "AUX3", "type": TYPE_DIMENSIONLESS, "unit": UNIT_NONE},
+        47408: {"name": "AUX4", "type": TYPE_DIMENSIONLESS, "unit": UNIT_NONE},
+        47407: {"name": "AUX5", "type": TYPE_DIMENSIONLESS, "unit": UNIT_NONE},
+        47412: {"name": "X7", "type": TYPE_DIMENSIONLESS, "unit": UNIT_NONE},
+    },
     "inverter_m8_status": {
         44703: {"name": "defrosting EB101", "type": TYPE_BOOLEAN_YES_NO, "unit": UNIT_NONE},
         44396: {"name": "pump speed heating medium GP1", "type": TYPE_PERCENTAGE, "unit": UNIT_PERCENT},
@@ -80,11 +103,15 @@ definition_groups = {
     "inverter_m8_compressor_module": {
         10014: {"name": "blocked", "type": TYPE_BOOLEAN_YES_NO, "unit": UNIT_NONE},
         44069: {"name": "compressor starts EB101", "type": TYPE_DIMENSIONLESS, "unit": UNIT_NONE},
+        44866: {"name": "current EB101", "type": TYPE_DIMENSIONLESS, "unit": UNIT_NONE},
         44702: {"name": "cpr. protection mode EB101", "type": TYPE_BOOLEAN_YES_NO, "unit": UNIT_NONE},
         44058: {"name": "condenser out EB101-BT12", "type": TYPE_TEMPERATURE, "unit": UNIT_CELSIUS},
         44363: {"name": "evaporator EB101-BT16", "type": TYPE_TEMPERATURE, "unit": UNIT_CELSIUS},
+        41167: {"name": "evaporator EB101-BT84", "type": TYPE_TEMPERATURE, "unit": UNIT_CELSIUS},
+        41163: {"name": "high pressure EB101-BP9", "type": TYPE_TEMPERATURE, "unit": UNIT_CELSIUS},
         44059: {"name": "hot gas EB101-BT14", "type": TYPE_TEMPERATURE, "unit": UNIT_CELSIUS},
         44060: {"name": "liquid line EB101-BT15", "type": TYPE_TEMPERATURE, "unit": UNIT_CELSIUS},
+        41162: {"name": "low pressure EB101-BP8", "type": TYPE_TEMPERATURE, "unit": UNIT_CELSIUS},
         44055: {"name": "return temp. EB101-BT3", "type": TYPE_TEMPERATURE, "unit": UNIT_CELSIUS},
         44061: {"name": "suction gas EB101-BT17", "type": TYPE_TEMPERATURE, "unit": UNIT_CELSIUS},
         44699: {"name": "high pressure sensor EB101", "type": TYPE_PRESSURE, "unit": UNIT_BAR},
@@ -119,6 +146,8 @@ def parse_raw_value(raw_value, type, unit):
         return float(raw_value[:-3])
     elif unit == UNIT_PERCENT:
         return int(raw_value[:-1])
+    elif unit == UNIT_CURRENCY:
+        return float(re.sub(r'^(\d+)(.*)$', r'\1', raw_value, flags=re.UNICODE))
 
     return raw_value
 
@@ -129,9 +158,11 @@ def get_definition_variables(definition_group):
 
 def augment_definition_values(definition_group, values):
     for value in values["Values"]:
-        type = definition_group[int(value["VariableId"])]["type"]
-        unit = definition_group[int(value["VariableId"])]["unit"]
-        definition_group[int(value["VariableId"])]["value"] = parse_raw_value(value["CurrentValue"], type, unit)
+        varid = int(value["VariableId"])
+        if varid in definition_group:
+          type = definition_group[varid]["type"]
+          unit = definition_group[varid]["unit"]
+          definition_group[varid]["value"] = parse_raw_value(value["CurrentValue"], type, unit)
 
     return definition_group
 
@@ -139,9 +170,14 @@ def augment_definition_values(definition_group, values):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--system_id", type=int, help="The system/heat pump ID")
+    parser.add_argument("-p", "--prefix", action='store_true', help="Prefix value with time; id; field name in CSV format")
+    parser.add_argument("-c", "--currvar", metavar='n', type=int, nargs='+', help="The system variables requested")
+
     args = parser.parse_args()
 
     system_id = args.system_id
+    currvar = args.currvar
+    pref = args.prefix
     email = os.getenv("EMAIL")
     password = os.getenv("PASSWORD")
 
@@ -158,24 +194,50 @@ if __name__ == "__main__":
     if redirect.headers.get("Location") != f"/System/{system_id}/Status/Overview":
         raise Exception("Authentication failed")
 
-    # Start fetching values, one "group" at a time
-    for definition_group_name in definition_groups.keys():
+    if not currvar:
+
+      # Start fetching values, one "group" at a time
+      for definition_group_name in definition_groups.keys():
         definition_group = definition_groups[definition_group_name]
 
         values_response = session.post("https://myupway.com/PrivateAPI/Values", {
-            "hpid": system_id,
-            "variables": get_definition_variables(definition_group)
+          "hpid": system_id,
+          "variables": get_definition_variables(definition_group)
         })
-
         values = values_response.json()
 
         # Augment the definition groups with the values received
         augmented_definition_group = augment_definition_values(definition_group, values)
         definition_groups[definition_group_name] = augmented_definition_group
 
-    metrics = {
+      metrics = {
         "system_id": system_id,
         "metrics": definition_groups
-    }
+      }
 
-    print(json.dumps(metrics, indent=4))
+      print(json.dumps(metrics, indent=2))
+
+    else:
+    # retrieve a single or a few current values
+    # EMAIL=a@b.c PASSWORD=pwd python3 ./get_metrics.py --system_id 123456 -currvar 44703 44396
+    # no
+    # 100
+
+      values_response = session.post("https://myupway.com/PrivateAPI/Values", {
+        "hpid": system_id,
+        "variables": currvar
+      })
+      values = values_response.json()
+
+      for cv in currvar:
+        for def_group_name in definition_groups.keys():
+          definition_group = definition_groups[def_group_name]
+          if cv in definition_group:
+            definition_group = augment_definition_values(definition_group, values)
+
+            if pref:
+             # CSV format
+              print (values["Date"] + ";" + str(cv) + ";" + definition_group[cv]["name"] +";" + str(definition_group[cv]["value"]))
+            else:
+            # single value
+              print (definition_group[cv]["value"])
